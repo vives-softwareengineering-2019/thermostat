59c94150e451fa8dbb50c5e8120e47f149377452
Sille Van Landschoot <info@sillevl.be>
2019-03-25 15:09:31 +0100

    add thermostat unit tests

---
+++ test/thermostat_units_test.rb
@@ -0,0 +1,85 @@
+require 'minitest/autorun'
+require 'json_thermostat'
+
+describe "unit support for thermostat" do
+
+  i_suck_and_my_tests_are_order_dependent!
+
+  it "should work without providing units" do
+    settings = JSON.generate({temperature: 20.0, range: 1.0})
+    measurement = JSON.generate({temperature: 20.0})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept settings in Celsius" do
+    settings = JSON.generate({temperature: 20.0, range: 1.0, unit: "celsius"})
+    measurement = JSON.generate({temperature: 20.0})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept settings in Fahrenheit" do
+    settings = JSON.generate({temperature: 68.0, range: 1.8, unit: "fahrenheit"})
+    measurement = JSON.generate({temperature: 20.0})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept settings in Kelvin" do
+    settings = JSON.generate({temperature: 293.15, range: 1.0, unit: "kelvin"})
+    measurement = JSON.generate({temperature: 20.0})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept measurements in Celsius" do
+    settings = JSON.generate({temperature: 20.0, range: 1.0})
+    measurement = JSON.generate({temperature: 20.0, unit: "celsius"})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept measurements in Fahrenheit" do
+    settings = JSON.generate({temperature: 20.0, range: 1.0})
+    measurement = JSON.generate({temperature: 68.0, unit: "fahrenheit"})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+  it "should accept measurements in Kelvin" do
+    settings = JSON.generate({temperature: 20.0, range: 1.0})
+    measurement = JSON.generate({temperature: 293.15, unit: "kelvin"})
+    expected_result = JSON.generate({cooling: false, heating: false})
+
+    thermostat = JSONThermostat.new settings
+    result = thermostat.update measurement
+
+    result.must_equal expected_result
+  end
+
+end
